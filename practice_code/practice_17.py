af9326b57f7c55a2036e509476d28eedc2d63934
kdhsea <kdhsea@gmail.com>
2020-12-25 14:37:23 +0900

    codeup_practice_1143,1144,1147-1150

---
+++ practice_code/practice_17.py
@@ -0,0 +1,63 @@
+# 1143 : 비트 연산자(AND)
+
+# 두 수를 입력받아 비트단위로 AND연산한 후 결과를 출력하시오
+
+# a, b = map(int, input().split())
+
+# print(a & b)
+
+# 1144 : 비트 연산자(OR)
+
+# 두 수를 입력받아 비트단위로 OR연산한 후 결과를 출력하시오.
+
+# a, b = map(int, input().split())
+
+# print(a|b)
+
+# 1147 : 비트 연산자(<<)
+
+# 정수 a, x를 입력받아 a의 값을 x만큼 SHIFT(<<) 연산한 후 결과를 출력하시오.
+
+# a, x = map(int, input().split())
+
+# print(a << x)
+
+# 1148 : 비트 연산자(>>)
+
+# a, x = map(int, input().split())
+
+# print(a >> x)
+
+# 1149 : 두 수 중 큰 수
+
+# 두 정수 중 큰 정수를 출력한다.
+
+# 입력
+# 두 정수가 입력된다.
+
+# 출력
+# 두 정수 중 큰 정수를 출력한다.
+
+# 입력 예시   
+# 2 7
+
+# 출력 예시
+# 7
+
+# a, b = map(int, input().split())
+
+# if a > b :
+#     print(a)
+
+# else :
+#     print(b)
+
+# 1150 : 세 수 중 가장 작은 수
+
+# 세 정수가 주어지면 그 중 가장 작은 수를 출력한다.
+
+# n = list(map(int, input().split()))
+
+# print(min(n))
+
+    
